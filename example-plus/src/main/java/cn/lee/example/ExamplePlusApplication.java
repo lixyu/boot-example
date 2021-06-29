dc3964e230af98ffd9231fbd7929719a499fc693
Lee <lee.x.li.-nd@disney.com>
2021-06-29 15:35:18 +0800

    no message

---
+++ example-plus/src/main/java/cn/lee/example/ExamplePlusApplication.java
@@ -0,0 +1,13 @@
+package cn.lee.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplePlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamplePlusApplication.class, args);
+    }
+
+}
---
+++ example-plus/src/test/java/cn/lee/example/ExamplePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lee.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplePlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
